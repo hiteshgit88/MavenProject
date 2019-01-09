f3e6073902bfc80cd0f72622aed4b20b0bcfadae
Crest <Crest@HiteshJHPJLenovoPP>
2019-01-09 17:48:49 +0530

    Commit 09-01-2019

--- MavenProject/src/main/java/tests/NewTest.java
+++ MavenProject/src/main/java/tests/NewTest.java
@@ -4,6 +4,8 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,11 +15,10 @@ import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.DataProvider;
@@ -405,13 +406,27 @@ public class NewTest {
   
 
   @AfterTest
+  @Parameters({"platformName","platformVersion","browserName"})
+  public static RemoteWebDriver getRemoteWebDriver(String platformName, String platformVersion, String browserName) throws MalformedURLException {
+
+		String host = "stag-app.mybrokerbee.com";
+		DesiredCapabilities capabilities = new DesiredCapabilities(browserName, "", Platform.ANY);
+
+		capabilities.setCapability("user", "temp_broker");
+		capabilities.setCapability("password", "temp");
+
+
+	RemoteWebDriver webdriver = new RemoteWebDriver(new URL("https://" + host), capabilities);
+
+		return webdriver;
+}
   @Parameters({"type","fileName"})
   public static void downloadReport(RemoteWebDriver driver, String type, String fileName) throws IOException {
 	  try { 
 			String command = "mobile:report:download"; 
 			Map<String, Object> params = new HashMap<String, Object>(); 
 			params.put("type", type); 
-			String report = (String)driver.executeScript(command, params); 
+			String report = (String)driver.executeAsyncScript(command, params); 
 			File reportFile = new File(fileName + "." + type); 
 			BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(reportFile)); 
 			byte[] reportBytes = OutputType.BYTES.convertFromBase64Png(report); 
